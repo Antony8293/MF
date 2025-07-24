d787ceb6ed28988d438854d4e585242e8a03b1ba
Antony8293 <anhvt93@gmail.com>
2025-07-24 13:58:04 +0700

    fix over line

--- Assets/Script/WarningLine.cs
+++ Assets/Script/WarningLine.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using Unity.Mathematics;
 using Unity.VisualScripting;
@@ -9,40 +10,84 @@ public class WarningLine : MonoBehaviour
     [SerializeField]
     private GameObject GameOverLine;
 
+     [SerializeField]
+    private GameObject GameWarningLine;
+
     private float time;
     private float TargetTime = 1.0f;
 
     private bool onWarning = false;
     private SpriteRenderer overLineRenderer;
+    [SerializeField]
+    private GameObject dynamicBox; // Reference to the dynamic box UI element
     private void Start()
     {
         if (GameOverLine != null)
         {
             overLineRenderer = GameOverLine.GetComponent<SpriteRenderer>();
         }
+
+        // Position warning line in upper 1/3 of dynamic box
+        PositionWarningLineInDynamicBox();
     }
-    public IEnumerator DelayCheckWarning()
+    private void PositionWarningLineInDynamicBox()
     {
-        yield return new WaitForSeconds(2f);
-        if (!onWarning)
+        if (dynamicBox != null && GameWarningLine != null)
+        {
+            RectTransform boxRect = dynamicBox.GetComponent<RectTransform>();
+            BoxCollider2D boxCollider = GameWarningLine.GetComponent<BoxCollider2D>();
+            if (boxRect != null && boxCollider != null)
+            {
+                // Get world corners of the dynamic box
+                Vector3[] corners = new Vector3[4];
+                boxRect.GetWorldCorners(corners);
+
+                // Calculate width and height in world units
+                float width = Vector3.Distance(corners[0], corners[3]); // left-bottom to left-top
+                float height = Vector3.Distance(corners[0], corners[1]); // left-bottom to right-bottom
+
+                // Calculate center position for upper 1/3
+                Vector3 topCenter = (corners[1] + corners[2]) / 2f;
+                Vector3 bottomCenter = (corners[0] + corners[3]) / 2f;
+                Vector3 regionCenter = Vector3.Lerp(topCenter, bottomCenter, 1f/6f); // 1/6 from top = center of upper 1/3
+
+                // Set collider size and position
+                boxCollider.size = new Vector2(width, height / 3f);
+                GameWarningLine.transform.position = regionCenter;
+                boxCollider.offset = Vector2.zero;
+                GameWarningLine.transform.rotation = Quaternion.identity;
+            }
+            else
+            {
+                Debug.LogWarning("dynamicBox hoặc GameWarningLine thiếu RectTransform hoặc BoxCollider2D!");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("dynamicBox hoặc GameWarningLine chưa được gán!");
+        }
+    }
+
+    public void CheckWarning()
+    {
+         if (!onWarning)
         {
             overLineRenderer.enabled = false;
             time = 0.0f;
-            Debug.Log("Exit Warning: " + Time.time);
+            //Debug.Log("Exit Warning: " + Time.time);
         }
-
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         onWarning = false;
-        StartCoroutine(DelayCheckWarning());
+        Invoke("CheckWarning", 2f);
        
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         onWarning = true;    
-        // Debug.Log("Enter Warning: " + collision.name + " at " + Time.time);
+        //Debug.Log("Enter Warning: " + collision.name + " at " + Time.time);
     }
 
     private void Update()
