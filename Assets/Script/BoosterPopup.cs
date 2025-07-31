2c0484ac3c55bddc1f3d8ada44b5351a426cbf4f
Antony8293 <anhvt93@gmail.com>
2025-07-31 10:52:02 +0700

    fix delayed booster

--- Assets/Script/BoosterPopup.cs
+++ Assets/Script/BoosterPopup.cs
@@ -33,8 +33,7 @@ public class BoosterPopup : MonoBehaviour
     public void Hide()
     {
         canvasGroup.DOFade(0f, 0.2f);
-        popupBox.DOScale(0f, 0.2f).SetEase(Ease.InBack)
-            .OnComplete(() => gameObject.SetActive(false));
+        popupBox.DOScale(0f, 0.2f).SetEase(Ease.InBack);
     }
 
     public void OnCloseClicked()
@@ -45,32 +44,34 @@ public class BoosterPopup : MonoBehaviour
     public void OnUseBoosterClicked(int boosterIndex)
     {
         if (boosterIndex < 0 || boosterIndex >= 4) return;
+        uiManager?.CloseCurrentPopup(); // Đóng popup sau
+        StartCoroutine(DelayInvokeBooster(boosterIndex));
+    }
 
+    private System.Collections.IEnumerator DelayInvokeBooster(int boosterIndex)
+    {
+        yield return new WaitForSeconds(0.3f);
         // Gọi hàm sử dụng booster tương ứng
         switch (boosterIndex)
         {
             case 0:
                 // Sử dụng booster Smallest
-                uiManager?.CloseCurrentPopup(); // Đóng popup sau
                 Booster.Booster1Clicked();
                 break;
             case 1:
                 // Sử dụng booster Hammer
-                uiManager?.CloseCurrentPopup(); // Đóng popup sau
                 Booster.Booster2Clicked();
                 break;
             case 2:
                 // Sử dụng booster Upgrade
-                uiManager?.CloseCurrentPopup(); // Đóng popup sau
                 Booster.Booster3Clicked();
                 break;
             case 3:
                 // Sử dụng booster Shake the Box
-                uiManager?.CloseCurrentPopup(); // Đóng popup sau
                 uiManager.UIScaleShakingBoosterEffect(Const.START_EFFECT); // Hiệu ứng scale
-                
-                
                 break;
         }
+                    
+        gameObject.SetActive(false);
     }
 }
