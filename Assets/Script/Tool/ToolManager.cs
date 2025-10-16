bc01c3d00875e0fbb878fd276177fa4449336e1d
Dinh Kiet Vy <dinhkietvy@gmail.com>
2025-10-16 12:43:37 +0700

    add load file

--- Assets/Script/Tool/ToolManager.cs
+++ Assets/Script/Tool/ToolManager.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public enum ToolType
@@ -7,9 +10,36 @@ public enum ToolType
     Destroy,
 }
 
+[System.Serializable]
+public class CrateData
+{
+    public string name;
+    public Vector3 position;
+
+    public CrateData(string name, Vector3 position)
+    {
+        this.name = name;
+        this.position = position;
+    }
+}
+
+[System.Serializable]
+public class LevelData
+{
+    public List<CrateData> crates = new List<CrateData>();
+    public LevelData(List<CrateData> crates)
+    {
+        this.crates = crates;
+    }
+
+    public LevelData() { }
+}
 
 public class ToolManager : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject Crates;
 
     public GameObject Crate;
 
@@ -21,8 +51,43 @@ public class ToolManager : MonoBehaviour
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             if(mousePos.y > 1.5f || mousePos.x < -1.7f || mousePos.x > 1.7f || mousePos.y < -1.8f) return;
-            mousePos.z = 0f; // ??t z v? 0 ?? ph? h?p v?i v? tr? 2D
-            Instantiate(Crate, mousePos, Quaternion.identity);
+            mousePos.z = 0f;
+            GameObject crate = Instantiate(Crate, mousePos, Quaternion.identity);
+            crate.transform.parent = Crates.transform;
+        }
+    }
+
+    public void SaveFile()
+    {
+        LevelData levelData = new LevelData();
+        foreach (Transform crate in Crates.transform)
+        {
+            //Debug.Log(crate.name);
+            CrateData cratedata = new CrateData("crate", crate.position);
+            levelData.crates.Add(cratedata);
+        }
+        string json = JsonUtility.ToJson(levelData);
+        string path = Application.persistentDataPath + "/level.json";
+        File.WriteAllText(path, json);
+        Debug.Log("?? l?u JSON t?i: " + path);
+    }
+
+    public void LoadFile()
+    {
+        string path = Application.persistentDataPath + "/level.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            LevelData levelData = JsonUtility.FromJson<LevelData>(json);
+            foreach (CrateData crateData in levelData.crates)
+            {
+                GameObject crate = Instantiate(Crate, crateData.position, Quaternion.identity);
+                crate.transform.parent = Crates.transform;
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Khong thay file");
         }
     }
 
